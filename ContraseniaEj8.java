5b5abf2759b370d123082a376b8bbe06300c4ab9
JJonson3 <163360787+JJonson3@users.noreply.github.com>
2026-07-17 19:12:28 -0500

    Update author name and preserve license comments

--- ContraseniaEj8.java
+++ ContraseniaEj8.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Johan David Auilar Cardenas
+ * @author Johan David Aguilar Cardenas
  */
 public class ContraseniaEj8 {
     public static void main(String[] args) {
