9aab307e5041f3f7c0ba6311dba31a473d8cafbf
JJonson3 <163360787+JJonson3@users.noreply.github.com>
2026-07-17 19:13:10 -0500

    Fix author name in PromedioEj10.java

--- PromedioEj10.java
+++ PromedioEj10.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Johan David Auilar Cardenas
+ * @author Johan David Aguilar Cardenas
  */
 public class PromedioEj10 {
     public static void main(String[] args) {
